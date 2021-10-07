4b1f971dd6a9de0d53aa3135ea6cacefde569c12
joaoOliveira <joaoavelino2008@hotmail.com>
2021-10-07 14:37:47 -0300

    refactoring

--- app.js
+++ app.js
@@ -1,62 +1,59 @@
 const pokemonUrl = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`;
-const filterInput = document.querySelector('#filter-pokemon');
-const listPokemons = document.getElementsByClassName('card');
-
+const filterInput = document.querySelector("#filter-pokemon");
+const listPokemons = document.getElementsByClassName("card");
 
 const fetchPokemon = () => {
-    const pokemonArray = [];
+  const pokemonArray = [];
 
-    for(let i = 1; i <= 151; i += 1) {
-        pokemonArray.push(fetch(pokemonUrl(i)).then(resp => resp.json()))
-    }
+  for (let i = 1; i <= 151; i += 1) {
+    pokemonArray.push(fetch(pokemonUrl(i)).then((resp) => resp.json()));
+  }
 
-    Promise.all(pokemonArray)
-        .then(pokemons => {
-
-            const listOfPokemons = pokemons.reduce((accumulator, pokemon) => {
-                const types = pokemon.types.map(typeInfo => typeInfo.type.name);
-                accumulator += 
-                `<li class="card ${types[0]}">
-                   <img class="card-image ${types[0]}" alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png">
+  Promise.all(pokemonArray).then((pokemons) => {
+    const listOfPokemons = pokemons.reduce((accumulator, pokemon) => {
+      const types = pokemon.types.map((typeInfo) => typeInfo.type.name);
+      accumulator += `<li class="card ${types[0]}">
+                   <img class="card-image ${types[0]}" alt="${
+        pokemon.name
+      }" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
+        pokemon.id
+      }.png">
                    <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
-                   <p class="card-subtitle">${types.join(' | ')}</p>
+                   <p class="card-subtitle">${types.join(" | ")}</p>
                 </li>
-                `
-                return accumulator;
-            }, '');
+                `;
+      return accumulator;
+    }, "");
 
-            const ul = document.querySelector('[data-js="pokedex"]');
+    const ul = document.querySelector('[data-js="pokedex"]');
 
-            ul.innerHTML = listOfPokemons;
-        })
-}
+    ul.innerHTML = listOfPokemons;
+  });
+};
 
 const searchPokemon = () => {
-    filterInput.addEventListener('input', (x) => {
-        console.log(x.target.value);
-    if(x.target.value.length > 0) {
-        for(let index = 0; index < listPokemons.length; index +=1){
-            const currentPokemon = listPokemons[index];
-            const pokemonName = currentPokemon.querySelector('.card-title');
-            const name = pokemonName.textContent;
-            console.log(name);
-            if(name != x.target.value) {
-                currentPokemon.classList.add('invisible');
-            } else {
-                currentPokemon.classList.remove('invisible');      
-            }
-         }
+  filterInput.addEventListener("input", (x) => {
+    console.log(x.target.value);
+    if (x.target.value.length > 0) {
+      for (let index = 0; index < listPokemons.length; index += 1) {
+        const currentPokemon = listPokemons[index];
+        const pokemonName = currentPokemon.querySelector(".card-title");
+        const name = pokemonName.textContent;
+        console.log(name);
+        if (name != x.target.value) {
+          currentPokemon.classList.add("invisible");
         } else {
-         for(let index = 0; index < listPokemons.length; index +=1) {
-             const currentPokemon = listPokemons[index];
-             currentPokemon.classList.remove('invisible');
-         }
+          currentPokemon.classList.remove("invisible");
+        }
+      }
+    } else {
+      for (let index = 0; index < listPokemons.length; index += 1) {
+        const currentPokemon = listPokemons[index];
+        currentPokemon.classList.remove("invisible");
+      }
     }
-     
-    });
-}
-
+  });
+};
 
 fetchPokemon();
 searchPokemon();
-    
